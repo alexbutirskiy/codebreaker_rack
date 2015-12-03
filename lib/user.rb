75a19361e7df0339a7cf72ecbfc70217ced36a48
Alex Butirskiy <butirskiy@gmail.com>
2015-12-03 13:36:18 +0200

    User class finished

--- lib/user.rb
+++ lib/user.rb
@@ -2,53 +2,78 @@ require 'rg_codebreaker'
 require 'yaml'
 
 class User
-  attr_accessor :name, :password
-  @@users = []
+  attr_accessor :name, :password, :created_at
 
   def self.find_by(fields = {})
     raise ArgumentError, 'wrong number of arguments (0 for 1+)' if fields.empty?
+
+    users = restore.select do |u|
+      fields.each do |k, v|
+        begin
+          break false unless u.public_send(k) == v
+        rescue NoMethodError
+          raise ArgumentError, "Attribute user.#{k} does not exist"
+        end
+      end
+    end
+
+    users.empty? ? nil : users.first
   end
 
   def self.save(users)
-    raise ArgumentError, 'argument must be an Array' unless users.kind_of? Array
+    raise ArgumentError, 'argument must be an Array' unless users.is_a? Array
     f = File.open('users.yaml', 'w')
     f.write users.to_yaml
     f.close
   end
 
   def self.restore
-    begin
-      f = File.open('users.yaml', 'r')
-      @@users = YAML.load(f.read)
-    rescue
-      @@users = []
-    ensure
-      f.close unless f.nil?
-    end
+    f = File.open('users.yaml', 'r')
+    YAML.load(f.read)
+  rescue
+    []
+  ensure
+    f.close unless f.nil?
   end
 
-  private_class_method :save
+  private_class_method :save, :restore
 
   def initialize(attributes = {})
     self.name = attributes[:name]
     self.password = attributes[:password]
+    self.created_at = Time.now
   end
 
   def save
-    if name.is_a?(String) and password.is_a?(String)
-      self.class.send :restore
-      @@users << self
-      self.class.send :save
-      true
-    else
-      false
+    return false if !name.is_a?(String) || name.empty?
+    return false if !password.is_a?(String) || password.empty?
+
+    users = self.class.send(:restore)
+
+    index = users.index { |a| a.name == name }
+
+    if index
+      if users[index].created_at == created_at
+        users.delete_at(index)
+      else
+        return false
+      end
     end
+
+    users << self
+    self.class.send(:save, users)
+    true
   end
 
   def destroy
-    self.class.send :restore
-    @@users << self
-    self.class.send :save
+    users = self.class.send :restore
+    users -= Array(self)
+    self.class.send(:save, users)
   end
 
+  def ==(other)
+    name == other.name &&
+      password == other.password &&
+      created_at == other.created_at
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 describe User do
-  after(:each) { User.class_variable_set(:@@users, []) }
-
   USER_NAME = 'User name'
   USER_PASSWORD = 'password'
-  USERS_TEST_ARR = [
-    { :name=>"P.McCartney", :password=>"BandOnTheRun" },
-    { :name=>"J.Lennon", :password=>"Imagine" }
-  ]
+  MCCARTNEY = 'P.McCartney'
+  LENNON = 'J.Lennon'
 
+  let(:lennon) { User.new(name: LENNON, password: 'Imagine') }
+  let(:mccartney) { User.new(name: MCCARTNEY, password: 'BandOnTheRun') }
+  let(:users_test_arr) { [] << lennon << mccartney }
 
   describe '.new class method' do
     it 'sets user name if "name" attribute provided' do
@@ -22,17 +21,6 @@ describe User do
     end
   end
 
-  describe '.find_by' do
-    it 'raise an ArgumentError if no argument provided' do
-      expect{ User.find_by }.to raise_exception ArgumentError
-    end 
-
-    xit 'raise an ArgumentError if wrong argument provided' do
-      expect{ User.find_by(missing: 'text') }.to raise_exception ArgumentError
-
-    end 
-  end
-
   describe '.save private class method' do
     before(:each) do
       @file_handle = double('File handle')
@@ -41,42 +29,81 @@ describe User do
 
     context 'when argument is not given' do
       it 'raises ArgumentError' do
-        expect{ User.send(:save) }.to raise_exception ArgumentError
+        expect { User.send(:save) }.to raise_exception ArgumentError
       end
     end
 
     context 'when argument is not an Array' do
       it 'raises ArgumentError' do
-        expect{ User.send(:save, {}) }.to raise_exception ArgumentError
+        expect { User.send(:save, {}) }.to raise_exception ArgumentError
       end
     end
 
     context 'when argument is an Array' do
       it 'saves given array to file "users.yaml" in yaml format' do
         expect(File).to receive(:open).with('users.yaml', 'w')
-        expect(@file_handle).to receive(:write).with(USERS_TEST_ARR.to_yaml)
+        expect(@file_handle).to receive(:write).with(users_test_arr.to_yaml)
         expect(@file_handle).to receive(:close)
-        User.send(:save, USERS_TEST_ARR)
+        User.send(:save, users_test_arr)
       end
     end
   end
 
-  # describe '.restore private class method' do
-  #   it 'restores @@users variable from file "users.yaml"' do
-  #     file_handle = double('File handle')
-  #     expect(File).to receive(:open).with('users.yaml', 'r').and_return(file_handle)
-  #     expect(file_handle).to receive(:read).and_return(USERS_TEST_ARR.to_yaml)
-  #     expect(file_handle).to receive(:close)
-  #     User.send :restore
-  #     expect(User.class_variable_get(:@@users)).to eq(USERS_TEST_ARR)
-  #   end
-  # end
+  describe '.restore private class method' do
+    it 'returns array of users loaded from file "users.yaml"' do
+      file_handle = double('File handle')
+      expect(File).to receive(:open).with('users.yaml', 'r').and_return(file_handle)
+      expect(file_handle).to receive(:read).and_return(users_test_arr.to_yaml)
+      expect(file_handle).to receive(:close)
+      expect(User.send(:restore)).to eq(users_test_arr)
+    end
+
+    it 'returns an empty array if "users.yaml" file abscent or broken' do
+      file_handle = double('File handle')
+      expect(File).to receive(:open).and_return(file_handle)
+      expect(file_handle).to receive(:read).and_raise(Errno::ENOENT)
+      expect(file_handle).to receive(:close)
+      expect(User.send(:restore)).to eq([])
+    end
+  end
+
+  describe '.find_by' do
+    before(:each) do
+      allow(User).to receive(:restore).and_return(users_test_arr)
+    end
+
+    context 'when "name" field provided and user exists' do
+      it 'returns object of the User class' do
+        expect(User.find_by(name: LENNON)).to be_a User
+      end
+
+      it 'returns requested object' do
+        user = User.find_by(name: LENNON)
+        expect(user.name).to eq LENNON
+      end
+    end
+
+    context 'when "name" field provided and user does not exist' do
+      it 'returns "nil"' do
+        user = User.find_by(name: 'Wrong name')
+        expect(user).to be_a_nil
+      end
+    end
+
+    it 'raises an ArgumentError if no argument provided' do
+      expect { User.find_by }.to raise_exception ArgumentError
+    end
+
+    it 'raises an ArgumentError if wrong field provided' do
+      expect { User.find_by(missing: 'text') }.to raise_exception ArgumentError
+    end
+  end
 
   let(:user) { User.new }
 
   it 'has a "name" attribute with full access' do
-    user.name = USER_NAME  
-    expect(user.name).to eq USER_NAME 
+    user.name = USER_NAME
+    expect(user.name).to eq USER_NAME
   end
 
   it 'has a "password" attribute with full access' do
@@ -84,51 +111,85 @@ describe User do
     expect(user.password).to eq USER_PASSWORD
   end
 
-  # describe '#save' do
+  describe '#save' do
+    before(:each) do
+      allow(User).to receive(:save)
+      allow(User).to receive(:restore).and_return(users_test_arr)
+    end
 
-  #   context 'when both "name" and "password" are defined' do
-  #     let(:user) { User.new(name: USER_NAME, password: USER_PASSWORD) }
+    context 'when both "name" and "password" are defined' do
+      let(:user) { User.new(name: USER_NAME, password: USER_PASSWORD) }
 
-  #     it 'calls User.restore' do
-  #       expect(User).to receive(:restore)
-  #       user.save
-  #     end
+      it 'calls User.restore' do
+        expect(User).to receive(:restore).and_return([])
+        user.save
+      end
 
-  #     it 'adds self to @@users dimension' do
-  #       user.save
-  #       expect(User.class_variable_get(:@@users).last).to eq user
-  #     end
+      it 'calls ".save" class method with array included "user"' do
+        expect(User).to receive(:save).with(array_including(user))
+        user.save
+      end
 
-  #     it 'calls User.save' do
-  #       expect(User).to receive(:save)
-  #       user.save
-  #     end
+      it 'returns "true"' do
+        allow(User).to receive(:save)
+        expect(user.save).to eq true
+      end
+    end
 
-  #     it 'returns "true"' do
-  #       expect(user.save).to eq true
-  #     end
-  #   end
+    context 'when "name" is not defined' do
+      let(:user) { User.new(password: USER_PASSWORD) }
 
-  #   context 'when "name" is not defined' do
-  #     let(:user) { User.new(password: USER_PASSWORD) }
+      it 'doesn\'t call User.save' do
+        expect(User).to_not receive(:save)
+        expect(user.save).to eq false
+      end
 
-  #     it 'doesn\'t call User.save' do
-  #       user = User.new(password: USER_NAME)
-  #       expect(User).to_not receive(:save)
-  #       expect(user.save).to eq false
-  #     end
+      it 'returns "false"' do
+        expect(user.save).to eq false
+      end
+    end
 
-  #     it 'returns "false"' do
-  #       expect(user.save).to eq false
-  #     end
-  #   end
-  # end
+    context 'when "password" is not defined' do
+      let(:user) { User.new(name: USER_NAME) }
 
-  # describe '.destroy' do
-  #   context 'when name is not defined' do
-  #     xit 'returns "false"' do
+      it 'doesn\'t call User.save' do
+        expect(User).to_not receive(:save)
+        expect(user.save).to eq false
+      end
 
-  #     end
-  #   end
-  # end
+      it 'returns "false"' do
+        expect(user.save).to eq false
+      end
+    end
+
+    context 'when user with the same name is already exist' do
+      it 'does not call ".save" class method' do
+        user = User.new(name: LENNON, password: USER_PASSWORD)
+        expect(User).to_not receive(:save)
+        user.save
+      end
+
+      it 'returns false' do
+        user = User.new(name: LENNON, password: USER_PASSWORD)
+        expect(user.save).to eq false
+      end
+    end
+  end
+
+  describe '.destroy' do
+    before(:each) do
+      allow(User).to receive(:save)
+      allow(User).to receive(:restore).and_return(users_test_arr)
+    end
+
+    it 'calls ".restore" class method' do
+      expect(User).to receive(:restore).and_return(users_test_arr)
+      lennon.destroy
+    end
+
+    it 'saves user list without current user' do
+      expect(User).to receive(:save).with(users_test_arr - Array(lennon))
+      lennon.destroy
+    end
+  end
 end
