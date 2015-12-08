09fb59f6c1b2d88e4f88238078a29d33e64f044d
Alex Butirskiy <butirskiy@gmail.com>
2015-12-08 16:50:45 +0200

    Safe cookies middleware frame added

---
+++ lib/safe_cookies.rb
@@ -0,0 +1,14 @@
+require 'openssl'
+
+class SafeCookies
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+
+    status, headers, body = @app.call(env)
+
+    [status, headers, body]
+  end
+end
